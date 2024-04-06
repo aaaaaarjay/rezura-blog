2414b13c87d86110b574239268aecfdacc49d926
aaaaaarjay <124123730+aaaaaarjay@users.noreply.github.com>
2024-04-06 13:42:00 +0800

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 
 import type { Metadata } from 'next'
-import { Raleway } from 'next/font/google'
+import { Aladin, Raleway } from 'next/font/google'
 import './globals.css'
 import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
 import Footer from "@/components/main/Footer";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react"
 
 
 
@@ -23,6 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Analytics />
   <SpeedInsights />
       <body className={`${raleway.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
       <StarsCanvas />
